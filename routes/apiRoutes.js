836209d1821e6146f1725fb3c9d96a782449001c
Sunny-Liao <sunnylia@gmail.com>
2019-04-05 12:42:34 -0500

    html layout handlebars & html routes created

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,23 +2,31 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  app.get("/api/tracker", function(req, res) {
+    db.Track.findAll({}).then(function(dbTrack) {
+      res.json(dbTrack);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/tracker", function(req, res) {
+    db.Track.create(req.body).then(function(dbTrack) {
+      res.json(dbTrack);
+    });
+  });
+
+  app.put("/api/tracker", function(req, res) {
+    db.Track.update(req.body).then(function(dbTrack) {
+      res.json(dbTrack);
+    }).catch(function(err) {
+      res.json(err);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  //app.delete("/api/examples/:id", function(req, res) {
+    //db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+      //res.json(dbExample);
+    //});
+  //});
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,19 +3,37 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Track.findAll({}).then(function(dbTrack) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        examples: dbTrack
       });
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
+  app.get("/mood", function(req, res) {
+    db.Track.findAll({}).then(function(dbTrack) {
+      res.render("mood", {
+        msg: "Welcome!",
+        examples: dbTrack
+      });
+    });
+  });
+
+  app.get("/coffee", function(req, res) {
+    db.Track.findAll({}).then(function(dbTrack) {
+      res.render("coffee", {
+        msg: "Welcome!",
+        examples: dbTrack
+      });
+    });
+  });
+
+  app.get("/drinks", function(req, res) {
+    db.Track.findAll({}).then(function(dbTrack) {
+      res.render("drinks", {
+        msg: "Welcome!",
+        examples: dbTrack
       });
     });
   });
